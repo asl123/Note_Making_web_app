5772ccbaed7d45a27a7c597467745520693e053e
Asloob Ali <asloobali123@gmail.com>
2020-07-20 23:06:41 +0530

    Updates

---
+++ precache-manifest.d9fe436f343937a07fd0829a0a74a9ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d856c441f66496879458481ac6727b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "de4ce2ba2b96afc4e59e",
+    "url": "/react-gh-pages/static/css/2.80468ea7.chunk.css"
+  },
+  {
+    "revision": "7546b89ed34a293a092a",
+    "url": "/react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de4ce2ba2b96afc4e59e",
+    "url": "/react-gh-pages/static/js/2.afe8b3b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.afe8b3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7546b89ed34a293a092a",
+    "url": "/react-gh-pages/static/js/main.30b34970.chunk.js"
+  },
+  {
+    "revision": "4a2d8fda25992c9e2287",
+    "url": "/react-gh-pages/static/js/runtime-main.c315ce7d.js"
+  }
+]);
